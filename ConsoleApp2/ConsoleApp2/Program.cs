a0d151261fc27df599b1da14ceb6bb97d49ed322
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-08 17:47:28 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void f()
+        {
+            try
+            {
+                Console.Write("Введите размер массива: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n <= 0) Console.WriteLine("Размер массива не может быть отрицательным или равным 0!!!");
+                else
+                {
+                    double[] arr = new double[n];
+                    Console.WriteLine("Введите числа: ");
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write("a[" + i + "] = ");
+                        arr[i] = Convert.ToDouble(Console.ReadLine());
+                    }
+
+                    double num = 0; //само максимальное число
+                    int number = 0; //номер максимального числа
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (arr[i] >= num)
+                        {
+                            num = arr[i];
+                            number = i + 1;
+                        }
+                    }
+                    Console.WriteLine("Номер последнего максимального числа - " + number);
+                }
+            }
+            catch { Console.WriteLine("Некорректный ввод данных!"); }
+        }
+
+        static void Main(string[] args)
+        {
+                f();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(textBox1.Text);
+                if (n <= 0) MessageBox.Show("Размер массива не может быть неположительным");
+                else
+                { 
+                    
+                    
+                        double[] arr = new double[n];
+                        string[] a = textBox2.Text.Split(' ');
+                        if (a.Length > n) MessageBox.Show("Вы ввели больше числе, чем необходиом, данные будут обрезаны!!!");
+                        for (int i = 0; i < n; i++)
+                        {
+                            arr[i] = Convert.ToDouble(a[i]);
+                        }
+
+                        double num = 0; //само максимальное число
+                        int number = 0; //номер максимального числа
+                        for (int i = 0; i < n; i++)
+                        {
+                            if (arr[i] >= num)
+                            {
+                                num = arr[i];
+                                number = i + 1;
+                            }
+                        }
+                        textBox3.Text = "Номер последнего максимального числа - " + number;
+                    
+                }
+            
+            }
+            catch { MessageBox.Show("Некорректный ввод данных"); }
+        }
+    }
+}
